913d6e831c216c522730f06b3a50a68337197404
Farhan-L4h <muhanmedan06@gmail.com>
2024-11-14 22:40:36 +0700

    login

--- config/auth.php
+++ config/auth.php
@@ -42,38 +42,17 @@ return [
         ],
         'api' => [
             'driver' => 'jwt',
-            'provider' => 'user',
+            'provider' => 'users', // Pastikan ini sama persis dengan nama provider di bagian 'providers'
         ],
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | User Providers
-    |--------------------------------------------------------------------------
-    |
-    | All authentication drivers have a user provider. This defines how the
-    | users are actually retrieved out of your database or other storage
-    | mechanisms used by this application to persist your user's data.
-    |
-    | If you have multiple user tables or models you may configure multiple
-    | sources which represent each model / table. These sources may then
-    | be assigned to any extra authentication guards you have defined.
-    |
-    | Supported: "database", "eloquent"
-    |
-    */
 
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => App\Models\UserModel::class, // Pastikan ini menunjuk ke model yang benar
         ],
-
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
     ],
+
 
     /*
     |--------------------------------------------------------------------------
